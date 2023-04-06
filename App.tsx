9a32321bd21b475ec997f4fbf1ca8e304f074ec2
Mehmet Yalınbaş <37973029+GruffDuck@users.noreply.github.com>
2023-04-06 16:28:38 +0300

    Finished

--- App.tsx
+++ App.tsx
@@ -1,20 +1,142 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import data from "./data";
+const viewWidth = Dimensions.get("window").width;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navbar}>
+        <Text style={styles.title}>Show Me The Coin</Text>
+        <Text style={styles.time}>29/10/2023, 1:20:23 PM</Text>
+
+        <Text
+          style={[
+            styles.navbuttons,
+            { alignSelf: "flex-start", paddingLeft: 20 },
+          ]}
+        >
+          Left
+        </Text>
+        <Text
+          style={[
+            styles.navbuttons,
+            { alignSelf: "flex-end", paddingRight: 20 },
+          ]}
+        >
+          
+          Right
+        </Text>
+      </View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <View style={styles.icon}></View>
+            <Text style={styles.nameTxt}>{item.name}</Text>
+            <Text style={styles.volume}>{item.volume}</Text>
+            <Text style={styles.price}>{item.price}</Text>
+            <Text style={styles.rank}>#{item.id}</Text>
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  navbar: {
+    backgroundColor: "#FFB4B4",
+    height: 100,
+    width: "100%",
+    justifyContent: "center",
+  },
+  title: {
+    paddingTop: 15,
+    color: "black",
+    fontSize: 20,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  navbuttons: {
+    position: "absolute",
+    color: "black",
+    fontSize: 15,
+    fontWeight: "400",
+    paddingTop: 35,
+  },
+  time: {
+    position: "absolute",
+    color: "black",
+    fontSize: 13,
+    fontWeight: "400",
+    paddingTop: 60,
+    alignSelf: "center",
+    textAlign: "center",
+  },
+  itemContainer: {
+    width: viewWidth,
+    height: 100,
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  icon: {
+    marginTop: 5,
+    width: 50,
+    height: 50,
+    backgroundColor: "red",
+    borderRadius: 25,
+    alignSelf: "flex-start",
+  },
+  nameTxt: {
+    position: "absolute",
+    color: "black",
+    marginLeft: 80,
+    marginTop: 15,
+    fontSize: 19,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+  },
+  volume: {
+    position: "absolute",
+    color: "black",
+    marginLeft: 80,
+    marginTop: 40,
+    fontSize: 15,
+    fontWeight: "300",
+    alignSelf: "flex-start",
+  },
+  price: {
+    position: "absolute",
+    color: "black",
+    marginLeft: 80,
+    marginTop: 60,
+    fontSize: 17,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+  },
+  rank: {
+    color: "black",
+    marginTop: -60,
+    textAlign: "center",
+    fontSize: 45,
+    fontWeight: "bold",
+    alignSelf: "flex-end",
   },
 });
---
+++ data.js
@@ -0,0 +1,70 @@
+const data = [
+  {
+    id: "1",
+    name: "Bitcoin",
+    volume: "12321421",
+    price: "23423.123",
+  },
+  {
+    id: "2",
+    name: "Ethereum",
+    volume: "3323121",
+    price: "3423.23",
+  },
+  {
+    id: "3",
+    name: "Doge",
+    volume: "12321421",
+    price: "3.13",
+  },
+  {
+    id: "4",
+    name: "Litecoin",
+    volume: "55213214213",
+    price: "0.982",
+  },
+  {
+    id: "5",
+    name: "Exen",
+    volume: "12321421223",
+    price: "0.423123",
+  },
+  {
+    id: "6",
+    name: "WinCoin",
+    volume: "12322221421",
+    price: "0.013",
+  },
+  {
+    id: "7",
+    name: "Bicoin",
+    volume: "12313121421",
+    price: "0.00123",
+  },
+  {
+    id: "8",
+    name: "Shiba",
+    volume: "12321321213141",
+    price: "0.012323",
+  },
+  {
+    id: "9",
+    name: "XRP",
+    volume: "12321421",
+    price: "23423.123",
+  },
+  {
+    id: "10",
+    name: "Bitci",
+    volume: "12321421",
+    price: "23.123",
+  },
+  {
+    id: "11",
+    name: "Holo",
+    volume: "12321421",
+    price: "23423.123",
+  },
+];
+export default data;
+
